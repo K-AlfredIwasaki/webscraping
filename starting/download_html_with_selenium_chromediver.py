dfeb86c1a5ef5642df707aa766a9ebfb654f0b6a
K-AlfredIwasaki <alfredki@umich.edu>
2017-08-29 22:01:30 -0700

    update

--- starting/download_html_with_selenium_chromediver.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-driver = webdriver.Chrome(executable_path = r'C:\Users\K\chromedriver_win32\chromedriver')
-
-driver.get('http://python.org')
-
-html_doc = driver.page_source
-
-print (html_doc)
--- starting/test.py
+++
@@ -1,7 +0,0 @@
-print ("print world")
-
-from selenium import webdriver
-
-webdriver.PhantomJS
-
-C:\Users\K\chromedriver_win32
